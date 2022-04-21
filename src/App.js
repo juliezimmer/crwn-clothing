f6a02577aca6dda6195c084e07076531e59fc63e
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2022-04-21 17:12:38 -0500

    added categories for clothing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import logo from "./logo.svg";
-import "./App.css";
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
 
-function App() {
-  return <div>Hello, World</div>;
-}
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          {/* <img /> */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
